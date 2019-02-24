eec3e7ad2d539a152c03c821492eeb47e146de17
t3kj3wlz <outlawstar4761@gmail.com>
2019-02-23 21:14:48 -0600

    first commit

---
+++ Messenger.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/Record/Record.php';
+require_once __DIR__ . '/SendMessage/SendMessage.php';
+
+abstract class Messenger{
+
+  public static function send($message){
+    return new SendMessage($message);
+  }
+}
